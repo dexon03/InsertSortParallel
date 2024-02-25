eb393af7cbb2cf5e9fdf77ac0604814afbe1c176
dexon03 <dima0958757803@gmail.com>
2024-02-25 12:24:08 +0200

    added base implementation

--- InsertSortParallel/Program.cs
+++ InsertSortParallel/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] arr = new int[5] { 8, 5, 7, 3, 1 };
+
+InsertionSort(arr);
+foreach (var ele in arr)
+{
+    Console.WriteLine(ele);
+}
+
+
+
+void InsertionSort(int[] arr)
+{
+    if (IsSorted(arr))
+    {
+        return;
+    }
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int j = i - 1;
+        int key = arr[i];
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+bool IsSorted(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
