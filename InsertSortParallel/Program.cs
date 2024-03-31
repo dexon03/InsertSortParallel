3527f7abed88745462550e9ab8bc095240c24b37
dexon03 <dima0958757803@gmail.com>
2024-03-31 12:46:03 +0300

    temp version of parallel version

--- InsertSortParallel/Program.cs
+++ InsertSortParallel/Program.cs
@@ -1,41 +1,86 @@
 ﻿
-int[] arr = new int[5] { 8, 5, 7, 3, 1 };
-
-InsertionSort(arr);
-foreach (var ele in arr)
+class Program
 {
-    Console.WriteLine(ele);
-}
-
-
-
-void InsertionSort(int[] arr)
-{
-    if (IsSorted(arr))
+    static void Main()
     {
-        return;
+        // List<int> arr = new List<int> { 5, 3, 8, 1, 2, 7, 4, 6 };
+        // int numThreads = 4;
+        // int[] arr = [5, 3, 8, 1, 2, 7, 4, 6];
+        // ParallelInsertionSort<int>(arr);
+        // foreach (var item in arr)
+        // {
+        //     Console.Write(item + " ");
+        // }
+        
     }
-    for (int i = 1; i < arr.Length; i++)
+
+    public static void ParallelInsertionSort<T>(T[] array) where T : IComparable<T>
     {
-        int j = i - 1;
-        int key = arr[i];
-        while (j >= 0 && arr[j] > key)
+        if (array.Length < 2)
         {
-            arr[j + 1] = arr[j];
-            j--;
+            return;
         }
-        arr[j + 1] = key;
-    }
-}
 
-bool IsSorted(int[] arr)
-{
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] < arr[i - 1])
+        // Divide the array into sub-arrays (assuming equal size for simplicity)
+        int subArraySize = array.Length / Environment.ProcessorCount;
+        List<T[]> subArrays = new List<T[]>();
+
+        for (int i = 0; i < array.Length; i += subArraySize)
         {
-            return false;
+            subArrays.Add(array.Skip(i).Take(subArraySize).ToArray());
+        }
+
+        // Parallel sorting of sub-arrays (ignoring potential overhead and synchronization concerns)
+        Parallel.ForEach(subArrays, subArray => InsertionSort(subArray));
+
+        // Merging using a temporary array and maintaining sorted order
+        T[] tempArray = new T[array.Length];
+        int mergedIndex = 0;
+        int currentSubArrayIndex = 0; // Index of the current sub-array
+
+        while (mergedIndex < tempArray.Length)
+        {
+            // Find the minimum element among available sub-arrays
+            T minElement = subArrays[currentSubArrayIndex][0];
+            int minSubArrayIndex = currentSubArrayIndex;
+
+            for (int i = 0; i < subArrays.Count; i++)
+            {
+                if (currentSubArrayIndex != i && subArrays[i].Length > 0 && subArrays[i][0].CompareTo(minElement) < 0)
+                {
+                    minElement = subArrays[i][0];
+                    minSubArrayIndex = i;
+                }
+            }
+
+            // Add the minimum element to the result and update sub-array
+            tempArray[mergedIndex++] = minElement;
+            subArrays[minSubArrayIndex] = subArrays[minSubArrayIndex].Skip(1).ToArray();
+
+            // Move to the next sub-array if it becomes empty
+            if (subArrays[minSubArrayIndex].Length == 0)
+            {
+                currentSubArrayIndex++;
+            }
+        }
+
+        Array.Copy(tempArray, array, array.Length);
+    }
+
+    private static void InsertionSort<T>(T[] array) where T : IComparable<T>
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            var current = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].CompareTo(current) > 0)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = current;
         }
     }
-    return true;
 }
