48962bf7faa383778be5c7ec1085debc6faf2b97
dexon03 <dima0958757803@gmail.com>
2024-04-15 14:24:33 +0300

    added parallel insertion sort

--- InsertSortParallel/Program.cs
+++ InsertSortParallel/Program.cs
@@ -1,86 +1,123 @@
-﻿
+﻿using System.Diagnostics;
+
 class Program
 {
     static void Main()
     {
-        // List<int> arr = new List<int> { 5, 3, 8, 1, 2, 7, 4, 6 };
-        // int numThreads = 4;
-        // int[] arr = [5, 3, 8, 1, 2, 7, 4, 6];
-        // ParallelInsertionSort<int>(arr);
-        // foreach (var item in arr)
-        // {
-        //     Console.Write(item + " ");
-        // }
-        
-    }
-
-    public static void ParallelInsertionSort<T>(T[] array) where T : IComparable<T>
-    {
-        if (array.Length < 2)
+        var arrLength = 1_000_000;
+        int[] arr = new int[arrLength];
+        int[] arr2 = new int[arrLength];
+        Random randNum = new Random();
+        for (int i = 0; i < arrLength; i++)
         {
-            return;
+            arr[i] = randNum.Next(0, 1000);
         }
 
-        // Divide the array into sub-arrays (assuming equal size for simplicity)
-        int subArraySize = array.Length / Environment.ProcessorCount;
-        List<T[]> subArrays = new List<T[]>();
+        Array.Copy(arr, arr2, arr.Length);
 
-        for (int i = 0; i < array.Length; i += subArraySize)
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ParallelInsertionSort(arr);
+        stopWatch.Stop();
+        Console.WriteLine("Time taken: " + stopWatch.ElapsedMilliseconds + "ms");
+        stopWatch.Start();
+        InsertionSort(arr2, 0, arrLength - 1);
+        stopWatch.Stop();
+        Console.WriteLine("Time taken: " + stopWatch.ElapsedMilliseconds + "ms");
+        //check correct result 
+        for (int i = 0; i < arrLength; i++)
         {
-            subArrays.Add(array.Skip(i).Take(subArraySize).ToArray());
-        }
-
-        // Parallel sorting of sub-arrays (ignoring potential overhead and synchronization concerns)
-        Parallel.ForEach(subArrays, subArray => InsertionSort(subArray));
-
-        // Merging using a temporary array and maintaining sorted order
-        T[] tempArray = new T[array.Length];
-        int mergedIndex = 0;
-        int currentSubArrayIndex = 0; // Index of the current sub-array
-
-        while (mergedIndex < tempArray.Length)
-        {
-            // Find the minimum element among available sub-arrays
-            T minElement = subArrays[currentSubArrayIndex][0];
-            int minSubArrayIndex = currentSubArrayIndex;
-
-            for (int i = 0; i < subArrays.Count; i++)
+            if(arr[i] != arr2[i])
             {
-                if (currentSubArrayIndex != i && subArrays[i].Length > 0 && subArrays[i][0].CompareTo(minElement) < 0)
-                {
-                    minElement = subArrays[i][0];
-                    minSubArrayIndex = i;
-                }
+                Console.WriteLine("Error");
+                break;
             }
-
-            // Add the minimum element to the result and update sub-array
-            tempArray[mergedIndex++] = minElement;
-            subArrays[minSubArrayIndex] = subArrays[minSubArrayIndex].Skip(1).ToArray();
-
-            // Move to the next sub-array if it becomes empty
-            if (subArrays[minSubArrayIndex].Length == 0)
+            if(i > 0 && (arr[i] < arr[i-1] || arr2[i] < arr[i-1]))
             {
-                currentSubArrayIndex++;
+                Console.WriteLine("Error");
+                break;
             }
         }
+        // var summary = BenchmarkRunner.Run<Benchmark>();
+    }
+    
+    private const int Threshold = 16;
 
-        Array.Copy(tempArray, array, array.Length);
+    public static void ParallelInsertionSort(int[] array)
+    {
+        ParallelInsertionSortInternal(array, 0, array.Length - 1);
     }
 
-    private static void InsertionSort<T>(T[] array) where T : IComparable<T>
+    private static void ParallelInsertionSortInternal(int[] array, int left, int right)
     {
-        for (int i = 1; i < array.Length; i++)
+        if (right - left + 1 <= Threshold)
         {
-            var current = array[i];
+            InsertionSort(array, left, right);
+        }
+        else
+        {
+            int mid = (left + right) / 2;
+            Task leftTask = Task.Run(() => ParallelInsertionSortInternal(array, left, mid));
+            Task rightTask = Task.Run(() => ParallelInsertionSortInternal(array, mid + 1, right));
+            Task.WaitAll(leftTask, rightTask);
+            Merge(array, left, mid, right);
+        }
+    }
+
+    public static void InsertionSort(int[] array, int left, int right)
+    {
+        for (int i = left + 1; i <= right; i++)
+        {
+            int key = array[i];
             int j = i - 1;
-
-            while (j >= 0 && array[j].CompareTo(current) > 0)
+            while (j >= left && array[j] > key)
             {
                 array[j + 1] = array[j];
                 j--;
             }
+            array[j + 1] = key;
+        }
+    }
 
-            array[j + 1] = current;
+    private static void Merge(int[] array, int left, int mid, int right)
+    {
+        int[] leftArray = new int[mid - left + 1];
+        int[] rightArray = new int[right - mid];
+
+        Array.Copy(array, left, leftArray, 0, mid - left + 1);
+        Array.Copy(array, mid + 1, rightArray, 0, right - mid);
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < leftArray.Length && j < rightArray.Length)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftArray.Length)
+        {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightArray.Length)
+        {
+            array[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 }
